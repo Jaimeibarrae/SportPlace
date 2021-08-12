e0ab55fdb476ccf5d8d87f3e88e6321f95220b94
DulceGarcia45 <dulcegarcia4545@gmail.com>
2021-08-12 03:55:35 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,12 +3,10 @@ import { config } from "dotenv";
 config();
 
 const configurations = {
-  PORT: process.env.PORT || 4000,
-  MONGODB_HOST: process.env.MONGODB_HOST || "localhost",
-  MONGODB_DATABASE: process.env.MONGODB_DB || "sportPlace",
-  MONGODB_URI: `mongodb://${process.env.MONGODB_HOST || "localhost"}/${
-    process.env.MONGODB_DATABASE || "sportPlace"
-  }`,
+  PORT: process.env.PORT || 3000,  
+  MONGODB_URI: process.env.MONGODB_URI  || "mongodb+srv://admin:admin1@sportplace.1l7vj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  
 };
+
 
 export default configurations;
